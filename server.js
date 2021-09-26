50ded0192bf6024c7d65b9bfbf02c81fae1e32d6
bobtokyo <tokyosocal@gmail.com>
2021-09-26 17:59:19 -0400

    updates

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express= require('express');
-const mongoose= require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 // const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const port = 8000;
+const app = express();
 
-const User=require('./models/User');
-mongoose.connect('mongodb://localhost/userData')
+const User = require('./models/User');
+let options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  dbName: "userData"
+};
+let uri = "mongodb://localhost:27017";
+mongoose.connect(uri, options);
 
 app.use(express.json());
 
@@ -14,15 +21,47 @@ app.listen(port, ()=>{
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
-})
+  app.post('/users',(req,res)=>{
+    User.create(
+      {
+        name:req.body.newData.name,
+        email:req.body.newData.email,
+        password:req.body.newData.password
+      },
+      (err,data)=>{
+      if (err){
+        res.json({success: false,message: err})
+      } else if (!data){
+        res.json({success: false,message: "Not Found"})
+      } else {
+        res.json({success: true,data: data})
+      }
+    })
+  })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
+
 // UPDATE
 .put((req,res)=>{
   // User.findByIdAndUpdate()
