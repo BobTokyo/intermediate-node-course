7035ee71335b0fa39e9ea024ddc6bc16f05aca11
bobtokyo <tokyosocal@gmail.com>
2021-10-03 11:32:35 -0400

    updates

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Intermediate Node.js
 
 Check out the issues of this repo for the course materials.
+
+Refactor =>
+client side req.body = 
+{
+  "newData":{
+    "name":"Jim",
+    "email":"jim@email.com",
+    "password":"newPassword"
+  }
+}
+mongoDB request = 
+ User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    {
+      new:true
+    },
+
+refactor mongoDB request using spread syntax = 
+ User.findByIdAndUpdate(
+    req.params.id,
+    {...req.body.newData},
+    {
+      new:true
+    },
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
-// const bodyParser= require('body-parser');
+// const bodyParser= require('body-parser'); //bodyParser deprecated
 const port = 8000;
 const app = express();
 
@@ -15,83 +16,72 @@ let uri = "mongodb://localhost:27017";
 mongoose.connect(uri, options);
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
+function testInfo(req){
+  console.log(`content-type = ${req.get('content-type')}`);
+  console.dir(`originalUrl = ${req.originalUrl}`);
+  console.dir(`path = ${req.path}`);
+}
+
+function sendResponse(res,err,data){
+  if (err){
+    res.json({
+      success: false,
+      message: err
+    })
+  } else if (!data){
+    res.json({
+      success: false,
+      message: "Not Found"
+    })
+  } else {
+    res.json({
+      success: true,
+      data: data
+    })
+  }
+}
+
+
 // CREATE
   app.post('/users',(req,res)=>{
     User.create(
-      {
-        name:req.body.newData.name,
-        email:req.body.newData.email,
-        password:req.body.newData.password
-      },
+      {...req.body.newData},
       (err,data)=>{
-      if (err){
-        res.json({success: false,message: err})
-      } else if (!data){
-        res.json({success: false,message: "Not Found"})
-      } else {
-        res.json({success: true,data: data})
+        sendResponse(res,err,data);
       }
-    })
+      )
   })
+  const filePathOptions = {
+    root: path.join(__dirname, '/files/')
+    }
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  User.findById(req.params.id,(err,data)=>{
-    if (err){
-      res.json({
-        success: false,
-        message: err
-      })
-    } else if (!data){
-      res.json({
-        success: false,
-        message: "Not Found"
-      })
-    } else {
-      res.json({
-        success: true,
-        data: data
-      })
+  testInfo(req);
+  User.findById(req.params.id,
+    (err,data)=>{
+    //  sendResponse(res,err,data);
+     res.sendFile('camera.png', filePathOptions);
     }
-  })
+    )
 })
 
 // UPDATE
 .put((req,res)=>{
   User.findByIdAndUpdate(
     req.params.id,
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
+    {...req.body.newData},
     {
       new:true
     },
-    (err,data)=>{
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found"
-        })
-      } else {
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
+    (err,data)=>{sendResponse(res,err,data)}
   )
 })
 
@@ -99,24 +89,7 @@ app.route('/users/:id')
 .delete((req,res)=>{
   User.findByIdAndDelete(
     req.params.id,
-    (err,data)=>{
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found"
-        })
-      } else {
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
+    (err,data)=>{sendResponse(res,err,data)}
   )
 })
 
